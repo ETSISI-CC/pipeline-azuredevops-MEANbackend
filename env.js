5b9be4c0bfa11a11020861a11af955a548b4e895
jdiazfernandez <yesica.diaz@upm.es>
2018-12-24 12:03:07 +0100

    fixed untracked files

--- env.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    accountName: 'test-connection', //Conexion name
-    databaseName: 'test-bd', //CosmosDB database name
-    key: 'test-key', // PRIMARY KEY
-    port: 10255 // Port of Azure
-  };

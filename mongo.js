acadf8d4eb54bd3c6a3fb8fab8aaaf21f12468a0
jdiazfernandez <yesica.diaz@upm.es>
2018-12-24 11:46:46 +0100

    Update mongo.js - envTest removed

--- mongo.js
+++ mongo.js
@@ -1,12 +1,5 @@
 const mongoose = require('mongoose');
-const testEnv = {
-  accountName: 'bd-demo-pfg', //Conexion name
-  databaseName: 'demo-bd', //CosmosDB database name
-  key: 'NyJVl8ZoO0sM4oNTEXVRq3SeztTk', // PRIMARY KEY
-  port: 10255 // Port of Azure
-};
-
-const env = require('./env') || testEnv;
+const env = require('./env');
 
 const mongoUri = `mongodb://${env.accountName}:${env.key}@${env.accountName}.documents.azure.com:${env.port}/${
    env.databaseName
